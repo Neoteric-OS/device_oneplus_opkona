c10df93fd9138082ccadf4d799fb6b0964781037
Dominik Procházka <dominikprochazka100@gmail.com>
2023-03-19 14:52:50 +0100

    opkona: Aperture support

--- device.mk
+++ device.mk
@@ -383,6 +383,7 @@ PRODUCT_PACKAGES += \
     
 # Overlays - OP8
 PRODUCT_PACKAGES += \
+    OnePlus8Aperture \
     OnePlus8FrameworkRes \
     OnePlus8WifiOverlay \
     OnePlus8SettingsProviderOverlay \
@@ -390,6 +391,7 @@ PRODUCT_PACKAGES += \
     
 # Overlays - OP8P
 PRODUCT_PACKAGES += \
+    OnePlus8ProAperture \
     OnePlus8ProFrameworkRes \
     OnePlus8ProWifiOverlay \
     OnePlus8ProSettingsProviderOverlay \
