74355a54ef8a74c4deadc8bd9a26d493338f1c9f
HELLBOY017 <abhaygill017@gmail.com>
2023-07-04 00:01:08 +0530

    opkona: DeviceExtras: Remove auto-hbm
    We now use auto hbm which is supported natively by aosp.

--- DeviceExtras/src/org/zephyrus/device/DeviceExtras/FileUtils.java
+++ DeviceExtras/src/org/zephyrus/device/DeviceExtras/FileUtils.java
@@ -39,26 +39,6 @@ public class FileUtils {
     private static final String TAG = "FileUtils";
 
     private static boolean mServiceEnabled = false;
-
-    private static void startService(Context context) {
-        context.startServiceAsUser(new Intent(context, AutoHBMService.class),
-                UserHandle.CURRENT);
-        mServiceEnabled = true;
-    }
-
-    private static void stopService(Context context) {
-        mServiceEnabled = false;
-        context.stopServiceAsUser(new Intent(context, AutoHBMService.class),
-                UserHandle.CURRENT);
-    }
-
-    public static void enableService(Context context) {
-        if (DeviceExtras.isAUTOHBMEnabled(context) && !mServiceEnabled) {
-            startService(context);
-        } else if (!DeviceExtras.isAUTOHBMEnabled(context) && mServiceEnabled) {
-            stopService(context);
-        }
-    }
     
     /**
      * Checks whether the given file exists
