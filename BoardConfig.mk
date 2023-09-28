7e34af725f979c60e1729aa545cdfedd22cb7b43
HELLBOY017 <abhaygill017@gmail.com>
2023-09-28 22:26:05 +0530

    opkona: Ship with KernelSU

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,8 +145,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
-KERNEL_DEFCONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig ksu.config
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
